3504c487934ced6142848c6f7d88ddf2dd1d275e
JetsadaWijit <jetsadawijit@outlook.com>
2024-08-17 05:41:48 +0000

    Change code

--- client.js
+++ client.js
@@ -1,15 +1,26 @@
-const express = require('express');
-
 require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const authRoutes = require('./routes/auth');
+const homeRoutes = require('./routes/home');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Routes
+app.use('/', authRoutes);
+app.use('/', homeRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
